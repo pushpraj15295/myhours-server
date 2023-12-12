e486f088f8a8c57470aa6f145b4b563ec994534f
pushpraj15295 <pushprajnda007@gmail.com>
2023-12-12 14:25:29 +0530

    login and sighup setup

--- index.js
+++ index.js
@@ -13,13 +13,19 @@ app.use(cors());
 app.use("/user", userController);
 app.use(authentication);
 
+app.get("/",(req,res)=>{
+  return res.status(200).json({
+    message:"welcome to MyHours"
+  })
+})
+
 app.listen(process.env.PORT, async () => {
   try {
     await connection;
     console.log("Connected to the database");
   } catch (error) {
     console.error("Unable to connect to the database. Error:", error.message);
-    process.exit(1); // Exit the process if unable to connect to the database
+    process.exit(1); 
   }
   console.log(`Server is listening on port ${process.env.PORT}`);
 });
