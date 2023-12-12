20172049a9b4d379d649df971356a70d2d2c22a9
pushpraj15295 <pushprajnda007@gmail.com>
2023-12-12 14:16:09 +0530

    login and sighup setup is done for server

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const { connection } = require("./config/db.js");
 const { userController } = require("./routes/user.routes");
 const { authentication } = require("./middlewares/authentication");
@@ -17,9 +16,10 @@ app.use(authentication);
 app.listen(process.env.PORT, async () => {
   try {
     await connection;
-    console.log("Connected to db");
-  } catch {
-    console.log("Unable to connect to db, Something went wrong!");
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Unable to connect to the database. Error:", error.message);
+    process.exit(1); // Exit the process if unable to connect to the database
   }
-  console.log(`Listening to ${process.env.PORT}`);
+  console.log(`Server is listening on port ${process.env.PORT}`);
 });
