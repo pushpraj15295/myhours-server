7d23e6f5986b4784223d3325598ae5bedd68b224
pushpraj15295 <pushprajnda007@gmail.com>
2023-12-12 14:48:45 +0530

    index rearranged and server connection changed

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,23 @@
-require("dotenv").config()
-const mongoose = require("mongoose");
-const connection = mongoose.connect(process.env.MONGOURL)
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectToDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGOURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log('Connected to the database');
+    return connection;
+  } catch (error) {
+    console.error('Unable to connect to the database. Error:', error.message);
+    process.exit(1); 
+  }
+};
 
 module.exports = {
-    connection
-}
+  connectToDatabase,
+};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 
-
-const { connection } = require("./config/db.js");
+const { connectToDatabase } = require("./config/db.js");
 const { userController } = require("./routes/user.routes.js");
 const { authentication } = require("./middlewares/authentication.js");
-
 
 app.use(express.json());
 app.use(cors());
@@ -16,15 +14,13 @@ app.get("/", (req, res) => {
   return res.status(200).json({
     message: "welcome to MyHours",
   });
-
 });
 app.use("/user", userController);
 app.use(authentication);
 
-
 app.listen(process.env.PORT, async () => {
   try {
-    await connection;
+    await connectToDatabase;
     console.log("Connected to the database");
   } catch (error) {
     console.error("Unable to connect to the database. Error:", error.message);
